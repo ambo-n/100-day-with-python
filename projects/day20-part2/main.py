bb6d58c09e96957c0ad72d2ed952ad12e7f37bc8
Amber <amber.quynh.nguyen@gmail.com>
2025-07-02 17:00:24 +0800

    day24

--- projects/day20-part2/main.py
+++ projects/day20-part2/main.py
@@ -35,11 +35,13 @@ while game_is_on:
     if snake.head.xcor() >280 or snake.head.xcor() <-280 or snake.head.ycor() >280 or snake.head.ycor() <-280:
         scoreboard.reset()
         snake.reset()
+        food.refresh()
     
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
             scoreboard.reset()
             snake.reset()
+            food.refresh()
 
 
 screen.exitonclick()
---
+++ projects/day20-part2/test.py
@@ -0,0 +1,4 @@
+with open("data.txt") as file:
+    number = file.read()
+    print(number)
+    print(type(number))
