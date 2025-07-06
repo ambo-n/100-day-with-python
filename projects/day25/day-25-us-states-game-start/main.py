6457e53d74fe1dcc8fa4695e6ce62e996fe4788d
Amber <amber.quynh.nguyen@gmail.com>
2025-07-06 21:23:54 +0800

    day26

--- projects/day25/day-25-us-states-game-start/main.py
+++ projects/day25/day-25-us-states-game-start/main.py
@@ -30,9 +30,6 @@ while game_is_on:
         guessboard.game_over()
         screen.exitonclick()
 
-state_to_learn =[]
-for state in data.state.values:
-    if state not in player_guesses:
-        state_to_learn.append(state)
+state_to_learn= [state for state in data.state.values if state not in player_guesses]
 new_data = pd.DataFrame(state_to_learn)
 new_data.to_csv("state_to_learn.csv", index=False)
