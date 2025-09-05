01f1f9c3fa4c540283120ac5a60e702ffbd3b577
Amber <amber.quynh.nguyen@gmail.com>
2025-09-05 21:39:33 +0800

    day49 part 2

--- day49/main.py
+++ day49/main.py
@@ -20,7 +20,7 @@ chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(GYM_URL)
 
-wait = WebDriverWait(driver, 2)
+wait = WebDriverWait(driver, 10)
 
 login_btn = wait.until(EC.element_to_be_clickable((By.ID, "login-button")))
 login_btn.click()
@@ -40,7 +40,7 @@ wait.until(EC.presence_of_element_located((By.ID,"schedule-page")))
 
 
 today = datetime.now()
-targeted_weekdays = [1,2]
+targeted_weekdays = [0,1]
 targeted_weekdays_in_datetime = [today+timedelta(days=((weekday-today.weekday() +7)%7) or 7) for weekday in targeted_weekdays]
 formatted_weekdays =[weekday.strftime("%a %b %d") for weekday in targeted_weekdays_in_datetime]
 
@@ -56,7 +56,7 @@ for weekday, formatted_weekday in zip(targeted_weekdays_in_datetime, formatted_w
     try:
         booking_button = driver.find_element(
             by=By.CSS_SELECTOR, 
-            value=f'div[id*="{weekday.date()}-1800"] button') # type: ignore
+            value=f'div[id*="{weekday.date()}-0700"] button') # type: ignore
         class_name = booking_button.find_element(
             By.XPATH, "ancestor::div/preceding-sibling::div/h3").text
         button_text = booking_button.text.strip()
@@ -72,21 +72,54 @@ for weekday, formatted_weekday in zip(targeted_weekdays_in_datetime, formatted_w
             if should_click:
                 booking_button.click()
                 time.sleep(0.5)
-            print(f"{msg}:{class_name} on {formatted_weekday}")
+            print(f"{msg}: {class_name} on {formatted_weekday}")
             counters[counter_key] += 1
             detailed_class_summary += f"[{summary_label.title()}] {class_name} on {formatted_weekday}\n"
         else:
             print(f"⚠ Unknown button state '{button_text}' for {class_name}")
     except NoSuchElementException:
         print("Can't find the button")
+
+
 total_classes = counters["new_booking"]+counters["waitlist"]+counters["already_booked"]
 
+# print(f"""
+# --- BOOKING SUMMARY ---
+# New Bookings           : {counters['new_booking']}
+# New Waitlist Entries   : {counters['waitlist']}
+# Already Booked/Waitlist: {counters['already_booked']}
+# Total Tue & Thu 6pm    : {total_classes}
+# """)
 print(f"""
---- BOOKING SUMMARY ---
-New Bookings           : {counters['new_booking']}
-New Waitlist Entries   : {counters['waitlist']}
-Already Booked/Waitlist: {counters['already_booked']}
-Total Tue & Thu 6pm    : {total_classes}
+--- Total Tue & Thu 6pm    : {total_classes} ---
 """)
 
-print(detailed_class_summary)
+#  --- Go To Booking Page ---
+
+driver.find_element(by=By.ID, value="my-bookings-link").click()
+wait.until(EC.presence_of_element_located((By.ID, "my-bookings-page")))
+
+all_cards = driver.find_elements(by=By.CSS_SELECTOR, value='[class*="MyBookings_bookingDetails"]')
+if not all_cards:
+    print("No classes found")
+else:
+    print("\n --- VERIFYING ON MY BOOKINGS PAGE ---")
+    for card in all_cards:
+        try:
+            h3 = card.find_element(By.TAG_NAME, "h3").text
+            print(f"✓ Verified: {h3}")
+        except NoSuchElementException:
+            print("No class name found")
+
+print(f"""
+--- VERIFICATION RESULT ---
+Expected: {total_classes} bookings
+Found: {len(all_cards)} bookings
+""")
+
+if total_classes == len(all_cards):
+    print("✅ SUCCESS: All bookings verified!")
+else:
+    print(f"❌ MISMATCH: Missing {total_classes-len(all_cards)} bookings")
+
+# print(detailed_class_summary)
